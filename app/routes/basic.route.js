510fbc0ce9d4106fbbb3528395f151d68adebb00
ChoHyunGook <whgusrnr7755@gmail.com>
2022-04-05 18:29:56 +0900

    2022-04-05 18:29

---
+++ app/routes/basic.route.js
@@ -0,0 +1,6 @@
+const { getBmi, getCalc } = require('../controllers/basic.controller');
+module.exports = x => x.app.post(`${x.url}/bmi`, getBmi) ;
+module.exports = x => x.app.post(`${x.url}/calc`, getCalc) ;
+
+
+  
