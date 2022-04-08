f8217ce5fee819b0c92acf93b3a7ad904831988b
ChoHyunGook <whgusrnr7755@gmail.com>
2022-04-08 17:17:32 +0900

    2022-04-08 17:17

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,14 +1,9 @@
-module.exports = mongoose =>{
-    const UserSchema = mongoose.model(
-        'user',
+module.exports = mongoose => mongoose.model('user',
         mongoose.Schema(
-            {
-                id: String,
-                password: String,
-                name: String,
-                telephone: String
+           {username: String,
+            password: String,
+            name: String,
+            telephone: String
             }, {timestamps: true}
         )
     )
-    return UserSchema
-}
